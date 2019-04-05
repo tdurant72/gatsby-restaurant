413e860edf1c7ca8ef2760ccf1504f087a33932d
Tyus Durant <contact@tyusdurant.com>
2019-04-05 15:47:27 -0400

    test variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   siteMetadata: {
     title: `Restaurant`,
@@ -32,5 +35,13 @@ module.exports = {
     // 'gatsby-plugin-offline',
     //my add ins
     `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.ACCESS_TOKEN,
+      },
+    }
   ],
 }
